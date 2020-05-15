624346af0ec9a04e7afa3bb8176d708f9a36006d
github username <github email address>
2020-05-15 01:06:56 -0500

    added async version

--- script.js
+++ script.js
@@ -19,9 +19,48 @@ const button = document.getElementById('btn')
 
 HTMLDocument.prototype.isDOMLoadedYet = new Promise((resolve) => {
   document.addEventListener('DOMContentLoaded', () => {
-    resolve({status: 'loaded and ready'})
+    resolve({'status': 'loaded and ready'})
   })
+}).catch(err => {
+  console.error(err)
 })
 
-document.isDOMLoadedYet
-  .then(status => console.log(status))
+// document.isDOMLoadedYet
+//   .then(status => console.log(status))
+//   .then(() => {
+//     button.addEventListener('click', () => {
+//       fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(data => data.json())
+//       .then(todo => console.log('todo :', todo))
+//     })
+//   })
+
+const app = async _ => {
+  const ready = await document.isDOMLoadedYet
+  console.log('ready: ', ready);
+
+  button.addEventListener('click', async () => {
+    const data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const todo = await data.json()
+    console.log('todo: ', todo);
+  })
+  
+}
+
+app()
+
+
+let done = false
+
+const isItDoneYet = new Promise((resolve, reject) => {
+  if (done) resolve("Yup, it is done")
+  else reject("Nope, still working on it")
+})
+
+const checkIfItIsDone = () => {
+  isItDoneYet
+  .then(yes => console.log(yes))
+  .catch(no => console.error(no))
+}
+
+checkIfItIsDone()
