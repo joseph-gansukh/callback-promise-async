d995d22af44f0a1f9c0eff3b32a09e6c7d3d4760
github username <github email address>
2020-05-19 23:53:56 -0500

    event-loop.js

---
+++ event-loop.js
@@ -0,0 +1,41 @@
+// Event Loop --> Call Stack
+
+// const bar = () => console.log('bar')
+// const baz = () => console.log('baz')
+
+// const foo = () => {
+//   console.log('foo')
+//   bar()
+//   baz()
+// }
+// // debugger
+// foo() // foo, bar, baz
+
+
+// Event Loop --> Message Queue
+// const bar = () => console.log('bar')
+// const baz = () => console.log('baz')
+
+// const foo = () => {
+//   console.log('foo')
+//   setTimeout(bar, 20000)
+//   baz()
+// }
+// debugger
+// foo() // foo, baz, bar
+
+
+// Event Loop --> Job Queue
+// const bar = () => console.log('bar')
+// const baz = () => console.log('baz')
+
+// const foo = () => {
+//   console.log('foo')
+//   setTimeout(bar, 5000)
+//   new Promise((resolve) =>
+//     resolve('should be right after baz, before bar')
+//   ).then(resolve => console.log(resolve))
+//   baz()
+// }
+// debugger
+// foo()
