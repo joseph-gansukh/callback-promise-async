92dd2b9626c443185cbe4b4b77da11c88a450bd4
github username <github email address>
2020-05-19 23:54:39 -0500

    updated event-loop

--- event-loop.js
+++ event-loop.js
@@ -1,41 +1,40 @@
-// Event Loop --> Call Stack
+Event Loop --> Call Stack
 
-// const bar = () => console.log('bar')
-// const baz = () => console.log('baz')
+const bar = () => console.log('bar')
+const baz = () => console.log('baz')
 
-// const foo = () => {
-//   console.log('foo')
-//   bar()
-//   baz()
-// }
-// // debugger
-// foo() // foo, bar, baz
-
-
-// Event Loop --> Message Queue
-// const bar = () => console.log('bar')
-// const baz = () => console.log('baz')
-
-// const foo = () => {
-//   console.log('foo')
-//   setTimeout(bar, 20000)
-//   baz()
-// }
-// debugger
-// foo() // foo, baz, bar
+const foo = () => {
+  console.log('foo')
+  bar()
+  baz()
+}
+foo() // foo, bar, baz
 
 
-// Event Loop --> Job Queue
-// const bar = () => console.log('bar')
-// const baz = () => console.log('baz')
+Event Loop --> Message Queue
+const bar = () => console.log('bar')
+const baz = () => console.log('baz')
 
-// const foo = () => {
-//   console.log('foo')
-//   setTimeout(bar, 5000)
-//   new Promise((resolve) =>
-//     resolve('should be right after baz, before bar')
-//   ).then(resolve => console.log(resolve))
-//   baz()
-// }
-// debugger
-// foo()
+const foo = () => {
+  console.log('foo')
+  setTimeout(bar, 20000)
+  baz()
+}
+debugger
+foo() // foo, baz, bar
+
+
+Event Loop --> Job Queue
+const bar = () => console.log('bar')
+const baz = () => console.log('baz')
+
+const foo = () => {
+  console.log('foo')
+  setTimeout(bar, 5000)
+  new Promise((resolve) =>
+    resolve('should be right after baz, before bar')
+  ).then(resolve => console.log(resolve))
+  baz()
+}
+debugger
+foo()
